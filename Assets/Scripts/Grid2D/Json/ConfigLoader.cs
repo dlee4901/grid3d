6ba482f88a7a50a0a78e82857152d10264a8295c
dlee4901 <dlee840@gatech.edu>
2026-03-10 21:15:04 -0500

    update json methods

--- Assets/Scripts/Grid2D/Json/ConfigLoader.cs
+++ Assets/Scripts/Grid2D/Json/ConfigLoader.cs
@@ -6,7 +6,7 @@ using Newtonsoft.Json;
 
 public static class ConfigLoader
 {
-    public static List<T> LoadFolder<T>(string folderPath, bool searchSubfolders=false) where T : INameId
+    public static List<T> LoadFolder<T>(string folderPath, bool searchSubfolders=false, bool polymorphic=false) where T : INameId
     {
         if (!Directory.Exists(folderPath))
             throw new DirectoryNotFoundException(folderPath);
@@ -14,19 +14,25 @@ public static class ConfigLoader
         // var objects = new List<T>();
         // var filePaths = searchSubfolders ? Directory.GetFiles(folderPath, "*.json", SearchOption.AllDirectories) : Directory.GetFiles(folderPath, "*.json");
         // foreach (var filePath in filePaths)
-        //     objects.Add(LoadFile<T>(filePath));
+        //     objects.Add(LoadFile<T>(filePath, polymorphic));
         // return objects;
         var filePaths = searchSubfolders ? Directory.GetFiles(folderPath, "*.json", SearchOption.AllDirectories) : Directory.GetFiles(folderPath, "*.json");
-        return filePaths.Select(LoadFile<T>).ToList();
+        return filePaths.Select(filePath => LoadFile<T>(filePath, polymorphic)).ToList();
     }
     
-    private static T LoadFile<T>(string filePath) where T : INameId
+    private static T LoadFile<T>(string filePath, bool polymorphic=false) where T : INameId
     {
         if (!File.Exists(filePath))
             throw new FileNotFoundException(filePath);
+        
+        var settings = !polymorphic ? null : new JsonSerializerSettings
+        {
+            Converters = new List<JsonConverter> { new NewtonsoftJsonConverter<T>() },
+            Formatting = Formatting.Indented
+        };
 
         var json = File.ReadAllText(filePath);
-        var obj = JsonConvert.DeserializeObject<T>(json) ?? throw new Exception($"Failed to parse file {filePath}");
+        var obj = JsonConvert.DeserializeObject<T>(json, settings) ?? throw new Exception($"Failed to parse file {filePath}");
         if (obj.Id == "") throw new Exception($"Missing ID in file {filePath}");
         
         return obj;
--- Assets/Scripts/Util/JsonHandler.cs
+++ Assets/Scripts/Util/JsonHandler.cs
@@ -1,21 +1,33 @@
+using System.Collections.Generic;
 using System.IO;
 using Newtonsoft.Json;
 using UnityEngine;
 
 public static class JsonHandler
 {
-    public static void SaveData<T>(T data) where T : INameId
+    public static void SaveData<T>(T data, bool polymorphic=false) where T : INameId
     {
+        var settings = !polymorphic ? null : new JsonSerializerSettings
+        {
+            Converters = new List<JsonConverter> { new NewtonsoftJsonConverter<T>() },
+            Formatting = Formatting.Indented
+        };
         var filePath = Path.Combine(Application.persistentDataPath, data.Id + ".json");
-        var jsonData = JsonConvert.SerializeObject(data); //JsonUtility.ToJson(data);
+        var jsonData = JsonConvert.SerializeObject(data, settings); //JsonUtility.ToJson(data);
         File.WriteAllText(filePath, jsonData);
     }
 
-    public static T LoadData<T>(string id) where T : INameId
+    public static T LoadData<T>(string id, bool polymorphic=false) where T : INameId
     {
-        string filePath = Path.Combine(Application.persistentDataPath, id + ".json");
-        if (!File.Exists(filePath)) throw new FileNotFoundException("Save file not found", filePath);
+        var settings = !polymorphic ? null : new JsonSerializerSettings
+        {
+            Converters = new List<JsonConverter> { new NewtonsoftJsonConverter<T>() },
+            Formatting = Formatting.Indented
+        };
+        var filePath = Path.Combine(Application.persistentDataPath, id + ".json");
+        if (!File.Exists(filePath))
+            throw new FileNotFoundException("Save file not found", filePath);
         var jsonData = File.ReadAllText(filePath);
-        return JsonConvert.DeserializeObject<T>(jsonData);
+        return JsonConvert.DeserializeObject<T>(jsonData, settings);
     }
 }
